2b069e4f27a2277c629bd27466af742aa207a063
Arkadiusz Placha <arekplacha24@gmail.com>
2019-01-20 10:36:45 +0100

    Added compression in test example.

--- sdram_test2/gen/gen_sdram_adapter.v
+++ sdram_test2/gen/gen_sdram_adapter.v
@@ -20,12 +20,19 @@ module gen_sdram_adapter(
     input    [3:0]  i_blue
 );
 
-reg     [7:0]   state           = 8'b00000001;
-reg     [7:0]   next_state      = 8'b00000001;
+parameter PIXEL_SIZE = 4;
+parameter CACHE_LENGTH = 128 / PIXEL_SIZE;
+
+reg     [9:0]   state           = 10'b0000000001;
+reg     [9:0]   next_state      = 10'b0000000001;
 reg             write_request;
 
 reg   [127:0]   cache1          = 128'b1;
 reg   [127:0]   cache2          = 128'b1;
+reg             cache_swt       = 1'b0;
+
+reg    [15:0]   counter         = 16'h0;
+reg             ctr_reset       = 1'b0;
 
 wire   [24:0]   current_pixel;
 wire   [63:0]   pixel_offset;
@@ -33,121 +40,171 @@ wire   [63:0]   current_bank;
 wire   [63:0]   future_bank;
 wire  [127:0]   offset_mask;
 wire            fetch_next;
+wire            cspace_fin;     
+
+wire            red;
+wire    [1:0]   green;
+wire            blue;
+
 
 assign current_pixel    = 640 * i_current_y + i_current_x;
-assign pixel_offset     = current_pixel % 10;
-assign current_bank     = (64'h1999999A * current_pixel) >> 32;
+assign pixel_offset     = current_pixel % CACHE_LENGTH;
+assign current_bank     = current_pixel / CACHE_LENGTH;
 assign future_bank      = (current_bank < 30720) ? current_bank + 1 : 0;
-assign fetch_next       = (pixel_offset == 0) && i_active_d;
+assign fetch_next       = (pixel_offset == 0);
 
 assign oreq             = write_request;
 assign oaddress         = future_bank;
-assign odata            = (state < 6'b001000) ? cache2 : cache1;
+assign odata            = (cache_swt) ? cache1 : cache2;
+assign cspace_fin       = (counter >= 2 * CACHE_LENGTH);
+
+assign red              = i_red[0] | i_red[1] | i_red[2] | i_red[3];
+assign green            = {i_green[0] | i_green[1], i_green[2] |  i_green[3]};
+assign blue             = i_blue[0] | i_blue[1] | i_blue[2] | i_blue[3];
+
+
+always @(negedge iclk_50 or posedge ctr_reset)
+begin
+    if(ctr_reset)
+        counter <= #1 16'h0;
+    else
+        counter <= #1 (counter + 1'b1);
+end
 
 always @(negedge iclk_50)
 begin
     if(ireset)
-        state <= #1 8'b00000001;
+        state <= #1 10'b0000000001;
     else
         state <= #1 next_state;
 end
 
-always @(state or fetch_next or iack)
+always @(state or fetch_next or iack or cspace_fin)
 begin
     case(state)
-        8'b00000001:
+        10'b0000000001:
             if(fetch_next)
-                next_state  <= 8'b00000010;
+                next_state  <= 10'b0000000010;
             else                
-                next_state  <= 8'b00000001;
-        8'b00000010:
-            next_state      <= 8'b00000100;
-        8'b00000100:
+                next_state  <= 10'b0000000001;
+        10'b0000000010:
+            next_state      <= 10'b0000000100;
+        10'b0000000100:
             if(iack)
-                next_state  <= 8'b00001000;
+                next_state  <= 10'b0000001000;
             else
-                next_state  <= 8'b00000010;
-        8'b00001000:
-            next_state      <= 8'b00010000;
+                next_state  <= 10'b0000000010;
+        10'b0000001000:
+            if(cspace_fin)
+                next_state  <= 10'b0000100000;
+            else
+                next_state  <= 10'b0000010000;
+        10'b0000010000:
+            next_state      <= 10'b0000001000;
             
-        8'b00010000:
+            
+        10'b0000100000:
             if(fetch_next)
-                next_state  <= 8'b00100000;
+                next_state  <= 10'b0001000000;
             else                
-                next_state  <= 8'b00010000;
-        8'b00100000:
-            next_state      <= 8'b01000000;
-        8'b01000000:
+                next_state  <= 10'b0000100000;
+        10'b0001000000:
+            next_state      <= 10'b0010000000;
+        10'b0010000000:
             if(iack)
-                next_state  <= 8'b10000000;
+                next_state  <= 10'b0100000000;
             else
-                next_state  <= 8'b00100000;
-        8'b10000000:
-            next_state      <= 8'b00000001;
+                next_state  <= 10'b0001000000;
+        10'b0100000000:
+            if(cspace_fin)
+                next_state  <= 10'b0000000001;
+            else
+                next_state  <= 10'b1000000000;
+        10'b1000000000:
+            next_state      <= 10'b0100000000;
         default:
-            next_state      <= 8'b00000001;
+            next_state      <= 10'b0000000001;
     endcase
 end
 
 always @(state)
 begin
     case(state)
-        8'b00000001:
+        10'b0000000001:
         begin
             write_request   <= #1 1'b0;
             
-            cache1          <= #1 cache1;
-            cache2          <= #1 cache2;
+            cache_swt       <= #1 1'b0;
+            ctr_reset       <= #1 1'b1;
         end        
-        8'b00000010:
+        10'b0000000010:
         begin
-            write_request   <= #1 1'b1; 
- 
-            cache1          <= #1 ((cache1 << 12) | {116'b0, i_red, i_green, i_blue});
-            cache2          <= #1 cache2;
-        end
-        8'b00000100:
-        begin
-            write_request   <= #1 1'b1;
+            write_request   <= #1 1'b1;  
             
-            cache1          <= #1 cache1;
-            cache2          <= #1 cache2;
+            cache1          <= #1 ((cache1 << PIXEL_SIZE) | {124'b0, red, green, blue});
+            cache_swt       <= #1 1'b0;
+            ctr_reset       <= #1 1'b0;
         end
-        8'b00001000:
+        10'b0000000100:
+        begin
+            write_request   <= #1 1'b1;            
+           
+            cache_swt       <= #1 1'b0;
+            ctr_reset       <= #1 1'b0;
+        end
+        10'b0000001000:
+        begin
+            write_request   <= #1 1'b0;            
+
+            cache1          <= #1 ((cache1 << PIXEL_SIZE) | {124'b0, red, green, blue});
+            cache_swt       <= #1 1'b0;
+            ctr_reset       <= #1 1'b0;
+        end
+        10'b0000010000:
         begin
             write_request   <= #1 1'b0;
             
-            cache1          <= #1 cache1;
-            cache2          <= #1 cache2;
+            cache_swt       <= #1 1'b0;
+            ctr_reset       <= #1 1'b0;
         end
         
-        8'b00010000:
+        
+        10'b0000100000:
         begin
             write_request   <= #1 1'b0;
             
-            cache1          <= #1 cache1;
-            cache2          <= #1 cache2;
+            cache_swt       <= #1 1'b1;
+            ctr_reset       <= #1 1'b1;
         end        
-        8'b00100000:
+        10'b0001000000:
+        begin
+            write_request   <= #1 1'b1;            
+            
+            cache2          <= #1 ((cache2 << PIXEL_SIZE) | {124'b0, red, green, blue});
+            cache_swt       <= #1 1'b1;
+            ctr_reset       <= #1 1'b0;
+        end
+        10'b0010000000:
         begin
             write_request   <= #1 1'b1;
             
-            cache1          <= #1 cache1;
-            cache2          <= #1 ((cache2 << 12) | {116'b0, i_red, i_green, i_blue});
+            cache_swt       <= #1 1'b1;
+            ctr_reset       <= #1 1'b0;
         end
-        8'b01000000:
+        10'b0100000000:
         begin
-            write_request   <= #1 1'b1;
+            write_request   <= #1 1'b0;            
             
-            cache1          <= #1 cache1;
-            cache2          <= #1 cache2;
-        end
-        8'b10000000:
+            cache2          <= #1 ((cache2 << PIXEL_SIZE) | {124'b0, red, green, blue});
+            cache_swt       <= #1 1'b1;
+            ctr_reset       <= #1 1'b0;
+        end 
+        10'b1000000000:
         begin
-            write_request   <= #1 1'b0;
-            
-            cache1          <= #1 cache1;
-            cache2          <= #1 cache2;
+            write_request   <= #1 1'b0;            
+           
+            cache_swt       <= #1 1'b1;
+            ctr_reset       <= #1 1'b0;
         end 
     endcase
 end
--- sdram_test2/vga/vga_sdram_adapter.v
+++ sdram_test2/vga/vga_sdram_adapter.v
@@ -20,12 +20,18 @@ module vga_sdram_adapter(
     output   [3:0]  o_blue
 );
 
-reg     [7:0]   state           = 8'b00000001;
-reg     [7:0]   next_state      = 8'b00000001;
+parameter PIXEL_SIZE = 4;
+parameter CACHE_LENGTH = 128 / PIXEL_SIZE;
+
+reg     [11:0]   state          = 12'b000000000001;
+reg     [11:0]   next_state     = 12'b000000000001;
 
 reg   [127:0]   cache1          = 128'b1;
 reg   [127:0]   cache2          = 128'b1;
 reg             cache_swt       = 1'b0;
+
+reg    [15:0]   counter         = 16'h0;
+reg             ctr_reset       = 1'b0;
 
 wire   [24:0]   current_pixel;
 wire    [3:0]   pixel_offset;
@@ -33,26 +39,35 @@ wire   [21:0]   current_bank;
 wire   [21:0]   future_bank;
 wire            fetch_next;
 reg             read_request;
+wire            cspace_fin;
 
 
 assign current_pixel    = 640 * i_current_y + i_current_x;
-assign pixel_offset     = current_pixel % 10;
-assign current_bank     = (64'h1999999A * current_pixel) >> 32;
+assign pixel_offset     = current_pixel % CACHE_LENGTH;
+assign current_bank     = current_pixel / CACHE_LENGTH;
 assign future_bank      = (current_bank < 30720) ? (current_bank + 1) : 0;
-
-assign fetch_next       = (pixel_offset == 0) && i_active_d;
+assign fetch_next       = (pixel_offset > 0);
 
 assign oread_req        = read_request;
 assign oread_address    = future_bank;
+assign cspace_fin       = (counter >= 2 * CACHE_LENGTH);
 
-assign o_red            = cache_swt ? cache1[11:8]  : cache2[11:8];
-assign o_green          = cache_swt ? cache1[7:4]   : cache2[7:4];
-assign o_blue           = cache_swt ? cache1[3:0]   : cache2[3:0];
+assign o_red            = cache_swt ? {cache1[3], cache1[3], cache1[3], cache1[3]} : {cache2[3], cache2[3], cache2[3], cache2[3]};
+assign o_green          = cache_swt ? {cache1[2], cache1[2], cache1[1], cache1[1]} : {cache2[2], cache2[2], cache2[1], cache2[1]};
+assign o_blue           = cache_swt ? {cache1[0], cache1[0], cache1[0], cache1[0]} : {cache2[0], cache2[0], cache2[0], cache2[0]};
+
+always @(negedge iclk_50 or posedge ctr_reset)
+begin
+    if(ctr_reset)
+        counter <= #1 16'h0;
+    else
+        counter <= #1 (counter + 1'b1);
+end
 
 always @(negedge iclk_50)
 begin
     if(ireset == 1'b1)
-        state <= #1 8'b00000001;
+        state <= #1 12'b000000000001;
     else
         state <= #1 next_state;
 end
@@ -60,92 +75,149 @@ end
 always @(state or fetch_next or iread_ack)
 begin
     case(state)
-        8'b00000001:
+        12'b000000000001:
             if(fetch_next)
-                next_state  <= 8'b00000010;
+                next_state  <= 12'b000000000010;
             else                
-                next_state  <= 8'b00000001;
-        8'b00000010:
-            next_state      <= 8'b00000100;
-        8'b00000100:
+                next_state  <= 12'b000000000001;
+        12'b000000000010:
+            next_state      <= 12'b000000000100;
+        12'b000000000100:
             if(iread_ack)
-                next_state  <= 8'b00001000;
+                next_state  <= 12'b000000001000;
             else
-                next_state  <= 8'b00000010;
-        8'b00001000:
-            next_state      <= 8'b00010000;
+                next_state  <= 12'b000000000010;
+        12'b000000001000:
+            next_state      <= 12'b000000010000;
+        12'b000000010000:
+            next_state      <= 12'b000000100000;
+        12'b000000100000:
+            if(cspace_fin)
+                next_state  <= 12'b000001000000;
+            else
+                next_state  <= 12'b000000010000;
             
-        8'b00010000:
+        12'b000001000000:
             if(fetch_next)
-                next_state  <= 8'b00100000;
+                next_state  <= 12'b000010000000;
             else                
-                next_state  <= 8'b00010000;
-        8'b00100000:
-            next_state      <= 8'b01000000;
-        8'b01000000:
+                next_state  <= 12'b000001000000;
+        12'b000010000000:
+            next_state      <= 12'b000100000000;
+        12'b000100000000:
             if(iread_ack)
-                next_state  <= 8'b10000000;
+                next_state  <= 12'b001000000000;
             else
-                next_state  <= 8'b00100000;
-        8'b10000000:
-            next_state      <= 8'b00000001;
+                next_state  <= 12'b000010000000;
+        12'b001000000000:
+            next_state      <= 12'b010000000000;
+        12'b010000000000:
+            next_state      <= 12'b100000000000;
+        12'b100000000000:
+            if(cspace_fin)
+                next_state  <= 12'b000000000001;
+            else
+                next_state  <= 12'b010000000000;
         default:
-            next_state      <= 8'b00000001;
+            next_state      <= 12'b000000000001;
     endcase
 end
 
 always @(state)
 begin
     case(state)
-        8'b00000001:
+        12'b000000000001:
         begin
             read_request    <= #1 1'b0;
             cache_swt       <= #1 1'b0;
+            
+            ctr_reset       <= #1 1'b1;
         end        
-        8'b00000010:
+        12'b000000000010:
         begin
             read_request    <= #1 1'b1;
             cache_swt       <= #1 1'b0;
             
+            cache2          <= #1 cache2 >> PIXEL_SIZE;
+            ctr_reset       <= #1 1'b0;
         end
-        8'b00000100:
+        12'b000000000100:
         begin
             read_request    <= #1 1'b1;
             cache_swt       <= #1 1'b0;
             
-            cache2          <= #1 cache2 >> 12;
+            ctr_reset       <= #1 1'b0;
         end
-        8'b00001000:
+        12'b000000001000:
         begin
             read_request    <= #1 1'b0;
-            cache_swt       <= #1 1'b0;
+            cache_swt       <= #1 1'b0;            
             
             cache1          <= #1 iread_data;
+            ctr_reset       <= #1 1'b0;
+        end
+        12'b000000010000:
+        begin
+            read_request    <= #1 1'b0;
+            cache_swt       <= #1 1'b0;
+            
+            cache2          <= #1 cache2 >> PIXEL_SIZE;
+            ctr_reset       <= #1 1'b0;
+        end
+        12'b000000100000:
+        begin
+            read_request    <= #1 1'b0;
+            cache_swt       <= #1 1'b0;
+            
+            
+            ctr_reset       <= #1 1'b0;
         end
         
-        8'b00010000:
+        
+        12'b000001000000:
         begin
             read_request    <= #1 1'b0;
             cache_swt       <= #1 1'b1;
+            
+            ctr_reset       <= #1 1'b1;
         end        
-        8'b00100000:
-        begin
-            read_request    <= #1 1'b1;
-            cache_swt       <= #1 1'b1;
-        end
-        8'b01000000:
+        12'b000010000000:
         begin
             read_request    <= #1 1'b1;
             cache_swt       <= #1 1'b1;
             
-            cache1          <= #1 cache1 >> 12;
+            cache1          <= #1 cache1 >> PIXEL_SIZE;
+            ctr_reset       <= #1 1'b0;
         end
-        8'b10000000:
+        12'b00010000000:
+        begin
+            read_request    <= #1 1'b1;
+            cache_swt       <= #1 1'b1;            
+            
+            ctr_reset       <= #1 1'b0;
+        end
+        12'b00100000000:
         begin
             read_request    <= #1 1'b0;
             cache_swt       <= #1 1'b1;
             
             cache2          <= #1 iread_data;
+            ctr_reset       <= #1 1'b0;
+        end
+        12'b01000000000:
+        begin
+            read_request    <= #1 1'b0;
+            cache_swt       <= #1 1'b1;
+            
+            cache1          <= #1 cache1 >> PIXEL_SIZE;
+            ctr_reset       <= #1 1'b0;
+        end
+        12'b10000000000:
+        begin
+            read_request    <= #1 1'b0;
+            cache_swt       <= #1 1'b1;            
+            
+            ctr_reset       <= #1 1'b0;
         end
     endcase
 end
